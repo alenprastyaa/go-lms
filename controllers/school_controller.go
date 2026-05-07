48289c623b372aef95de645f9e94179d54e8a4d9
alenprastyaa <1001cerita1001@gmail.com>
2026-05-07 16:04:10 +0700

    push

--- controllers/school_controller.go
+++ controllers/school_controller.go
@@ -11,19 +11,29 @@ import (
 )
 
 func (a *AppContext) CreateSchool(c *fiber.Ctx) error {
-	var body struct {
-		Name string `json:"name"`
+	name := strings.TrimSpace(c.FormValue("name"))
+	if name == "" {
+		var body struct {
+			Name string `json:"name"`
+		}
+		if err := c.BodyParser(&body); err != nil {
+			return utils.Error(c, 400, "Invalid request body")
+		}
+		name = strings.TrimSpace(body.Name)
 	}
-	if err := c.BodyParser(&body); err != nil {
-		return utils.Error(c, 400, "Invalid request body")
-	}
-
-	name := strings.TrimSpace(body.Name)
 	if name == "" {
 		return utils.Error(c, 400, "Nama sekolah wajib diisi")
 	}
 
 	school := models.School{Name: name}
+	if file, err := c.FormFile("logo"); err == nil && file != nil {
+		logoURL, upErr := utils.SaveUploadedFile(c, file)
+		if upErr != nil {
+			return utils.Error(c, 500, "Gagal upload avatar sekolah", upErr.Error())
+		}
+		school.LogoURL = &logoURL
+	}
+
 	if err := a.DB.Create(&school).Error; err != nil {
 		return utils.Error(c, 500, "Gagal membuat sekolah", err.Error())
 	}
@@ -43,24 +53,40 @@ func (a *AppContext) GetSchools(c *fiber.Ctx) error {
 
 func (a *AppContext) UpdateSchool(c *fiber.Ctx) error {
 	id := c.Params("id")
-	var body struct {
-		Name string `json:"name"`
-	}
-	if err := c.BodyParser(&body); err != nil {
-		return utils.Error(c, 400, "Invalid request body")
-	}
-
-	name := strings.TrimSpace(body.Name)
-	if name == "" {
-		return utils.Error(c, 400, "Nama sekolah wajib diisi")
-	}
-
 	var school models.School
 	if err := a.DB.Where("id = ?", id).First(&school).Error; err != nil {
 		return utils.Error(c, 404, "Sekolah tidak ditemukan")
 	}
 
-	if err := a.DB.Model(&school).Update("name", name).Error; err != nil {
+	name := strings.TrimSpace(c.FormValue("name"))
+	if name == "" {
+		var body struct {
+			Name string `json:"name"`
+		}
+		if err := c.BodyParser(&body); err != nil {
+			return utils.Error(c, 400, "Invalid request body")
+		}
+		name = strings.TrimSpace(body.Name)
+	}
+	if name == "" {
+		return utils.Error(c, 400, "Nama sekolah wajib diisi")
+	}
+
+	updates := map[string]interface{}{
+		"name": name,
+	}
+	if strings.EqualFold(strings.TrimSpace(c.FormValue("remove_logo")), "true") {
+		updates["logo_url"] = nil
+	}
+	if file, err := c.FormFile("logo"); err == nil && file != nil {
+		logoURL, upErr := utils.SaveUploadedFile(c, file)
+		if upErr != nil {
+			return utils.Error(c, 500, "Gagal upload avatar sekolah", upErr.Error())
+		}
+		updates["logo_url"] = logoURL
+	}
+
+	if err := a.DB.Model(&school).Updates(updates).Error; err != nil {
 		return utils.Error(c, 500, "Gagal memperbarui sekolah", err.Error())
 	}
 
